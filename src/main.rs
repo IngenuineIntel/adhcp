1722a23f8719a940e4478bc7a156dfe0cf35e909
IngenuineIntel <roan.rothrock@proton.me>
2025-02-17 21:52:52 -0600

    Temporarily ditched input monitoring

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 const VERSION: &'static str = "0.0.1";
 
 use std::sync::mpsc::Receiver;
+use std::{thread, time};
 use std::vec;
 
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
@@ -15,16 +16,17 @@ use ratatui::{
     widgets::{Block, Borders, Paragraph},
 };
 
-mod thread;
+mod threads;
 
-static STYLE: u8 = 1;
+static STYLE: u8 = 0;
+const REFRESH_RATE: u8 = 5;
 
 fn main() -> std::io::Result<()> {
     let mut terminal = ratatui::init();
 
     let log_receiver: Receiver<String>;
     let lease_receiver: Receiver<String>;
-    (log_receiver, lease_receiver) = thread::start_collection_thread(); 
+    (log_receiver, lease_receiver) = threads::start_collection_thread(); 
 
     let result = run(&mut terminal, log_receiver, lease_receiver);
     ratatui::restore();
@@ -36,6 +38,8 @@ fn run(terminal: &mut ratatui::DefaultTerminal,
     lease_receiver: Receiver<String> 
     ) -> std::io::Result<()> {
 
+    let frame_duration = time::Duration::from_millis(1000/(REFRESH_RATE as u64));
+
     let mut journal_logs = vec::Vec::new();
 
     loop {
@@ -46,10 +50,18 @@ fn run(terminal: &mut ratatui::DefaultTerminal,
         if handle_events()? {
             break Ok(());
         }
+        // if events {
+        //     do_events();
+        //     continue
+        // } else {
+        thread::sleep(frame_duration);
+        // }
     }
 }
 
 fn handle_events() -> std::io::Result<bool> {
+    // TODO fix input method to prevent hold
+    /*
     match event::read()? {
         Event::Key(key) if key.kind == KeyEventKind::Press => match key.code {
             KeyCode::Char('q') => return Ok(true),
@@ -59,6 +71,7 @@ fn handle_events() -> std::io::Result<bool> {
         // handle other events
         _ => {}
     }
+    */
     Ok(false)
 }
 
@@ -69,9 +82,10 @@ fn style_default() -> (Style, Style, Style) {
         .add_modifier(Modifier::BOLD);
     let title_style = Style::new()
         .light_blue();
-    let default_text_style = Style::new();
+    let text_style = Style::new()
+        .white();
 
-    (border_style, title_style, default_text_style)
+    (border_style, title_style, text_style)
 }
 
 fn style_matcha() -> (Style, Style, Style) {
@@ -82,10 +96,10 @@ fn style_matcha() -> (Style, Style, Style) {
     let title_style = Style::new()
         .green()
         .add_modifier(Modifier::BOLD);
-    let default_text_style = Style::new()
+    let text_style = Style::new()
         .add_modifier(Modifier::BOLD);
     
-    (border_style, title_style, default_text_style)
+    (border_style, title_style, text_style)
 }
 
 fn draw(frame: &mut Frame, journal_logs: vec::Vec<String>) {
@@ -93,17 +107,17 @@ fn draw(frame: &mut Frame, journal_logs: vec::Vec<String>) {
 
     let border_style: Style;
     let title_style: Style;
-    let default_text_style: Style;
+    let text_style: Style;
 
     match STYLE {
-        1 => {(border_style, title_style, default_text_style) = style_matcha();}
-        _ => {(border_style, title_style, default_text_style) = style_default();}
+        1 => {(border_style, title_style, text_style) = style_matcha();}
+        _ => {(border_style, title_style, text_style) = style_default();}
     }
 
     let version_nr_size: u16 = (VERSION.chars().count() as u16) + 2;
 
     let vertical = Layout::vertical([Length(1), Min(0), Length(10)]);
-    let [title_area, main_area, status_area] = vertical.areas(frame.area());
+    let [title_area, main_area, log_area] = vertical.areas(frame.area());
     let horizontal = Layout::horizontal([Fill(1); 2]);
     let title_horizontal = Layout::horizontal([Min(0), Length(version_nr_size)]);
     let [left_area, right_area] = horizontal.areas(main_area);
@@ -113,22 +127,20 @@ fn draw(frame: &mut Frame, journal_logs: vec::Vec<String>) {
         .iter()
         .rev()
         .map(|log| {
-            Line::from(Span::styled(format!("{}", log), default_text_style))
+            Line::from(Span::styled(format!("{}", log), text_style))
         }).collect();
 
     frame.render_widget(Paragraph::new(log_entries)
         .block(
-            Block::default()
+            Block::bordered()
                 .title("Logs")
                 .title_style(title_style)
-                .borders(Borders::ALL)
-                .border_style(border_style)
-        ), status_area
+                .style(border_style)
+        ), log_area
     );
 
     frame.render_widget(Block::bordered().title("Spyre").style(border_style), left_title_area);
     frame.render_widget(Block::bordered().title(VERSION).style(border_style), right_title_area);
-    //frame.render_widget(Block::bordered().title("Log").style(border_style), status_area);
     frame.render_widget(Block::bordered().title("Left").style(border_style), left_area);
     frame.render_widget(Block::bordered().title("Right").style(border_style), right_area);
 }
---
+++ src/threads.rs
@@ -0,0 +1,53 @@
+// spyre
+// thread.rs
+
+use std::{fs, path, process, thread, time};
+use std::io::{self, BufRead, BufReader};
+use std::sync::mpsc;
+
+pub fn start_collection_thread() -> (mpsc::Receiver<String>, mpsc::Receiver<String>) {
+    let (log_sender, log_receiver) = mpsc::channel();
+    let (lease_sender, lease_receiver) = mpsc::channel();
+
+    thread::spawn(move || {
+        collection_thread(log_sender, lease_sender);
+    });
+
+    (log_receiver, lease_receiver)
+}
+
+fn collection_thread(log_sender: mpsc::Sender<String>, lease_sender: mpsc::Sender<String>) {
+
+    const COLLECTION_REFRESH_RATE: u8 = 1;
+    let cap_duration = time::Duration::from_millis(1000/(COLLECTION_REFRESH_RATE as u64));
+
+    let mut leases_data = String::new();
+    let leases_path = path::Path::new("/var/lib/dhcpd/dhcpd.leases");
+
+    loop {
+
+        let mut fd = fs::File::open("/var/lib/dhcpd/dhcpd.leases");
+
+        let mut cmd = process::Command::new("journalctl")
+            .args(&["-fu", "dhcpd", "--no-pager"])
+            .stdout(process::Stdio::piped())
+            .spawn()
+            .expect("Failed to execute journalctl");
+
+        let stdout = cmd.stdout.take().expect("Failed to capture stdout");
+        let reader = BufReader::new(stdout);
+
+        // TODO check expirations and shit
+        
+        for line in reader.lines() {
+            if let Ok(line) = line {
+               let _ = log_sender.send(line.clone());
+            }
+        }
+        
+        thread::sleep(cap_duration);
+    }
+}
+
+
+
